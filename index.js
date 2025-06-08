4d9ebd7b2cc05e537bb251972021eb7911eb77c9
soumen0818 <sdas721444@gmail.com>
2025-06-08 13:18:11 +0530

    Refactor ticket handling: implement ticket creation and retrieval, integrate AI analysis, and set up email notifications for assigned moderators

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-// import { serve } from "inngest/express";
+import { serve } from "inngest/express";
 import userRoutes from "./routes/user.js";
-// import ticketRoutes from "./routes/ticket.js";
-// import { inngest } from "./inngest/client.js";
-// import { onUserSignup } from "./inngest/functions/on-signup.js";
-// import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
+import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/signup.js";
+import { onTicketCreated } from "./inngest/functions/ticket-create.js";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -20,13 +20,13 @@ app.use(express.json());
 app.use("/api/auth", userRoutes);
 app.use("/api/tickets", ticketRoutes);
 
-// app.use(
-//   "/api/inngest",
-//   serve({
-//     client: inngest,
-//     functions: [onUserSignup, onTicketCreated],
-//   })
-// );
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated],
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
